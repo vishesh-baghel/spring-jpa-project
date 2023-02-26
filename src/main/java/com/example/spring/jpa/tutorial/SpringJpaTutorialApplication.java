f42463e98209401a6d0245bc1eb7e89c763d0f4b
visheshbaghel <vishesh.baghel@batonsystems.com>
2023-02-26 16:23:53 +0530

    initial commit

---
+++ src/main/java/com/example/spring/jpa/tutorial/SpringJpaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.jpa.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaTutorialApplication.class, args);
+	}
+
+}
